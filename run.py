e9294acc6719b2d03ee33a73cb67ebf080a682e2
Felix Lehmann <felix_lehmann@ymail.com>
2024-12-08 23:25:09 +0000

    feat: add class 'investment_game' to run.py

--- run.py
+++ run.py
@@ -1,3 +1,51 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+import time
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+]
+
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("investment_game")
+
+secs = SHEET.worksheet("securities")
+data = secs.get_all_values()
+
+class investment_game():
+    """
+    the class of the game to be played
+    """
+    def __init__(self):
+        self.rounds = 5
+        self.ptf_amount = 1000.00
+        self.difficulty = ""
+        self.rounds_data = {}
+
+    def set_difficulty(self):
+        """
+        The player choses either 'easy', 'medium' or 'hard' mode as a game difficulty
+        """
+        diff = input("")
+
+    def start(self):
+        """
+        Method that welcomes the player, starts the game and fills the dictionary with data each round
+        """
+        print("")
+        self.difficulty = self.set_difficulty()
+
+def print_with_attention(str):
+    """
+    function that prints a text slowly to gather attention - I like it 
+    """
+    for i in str:
+        print(i, end=" ")
+        time.sleep(0.5)
+    print()
+
+print_with_attention("Print me with attention!")
