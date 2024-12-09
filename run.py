51c24649e3db458073079d2b76f33e6860988f22
Felix Lehmann <felix_lehmann@ymail.com>
2024-12-09 02:45:01 +0000

    feat: add investment-game class and menu within it

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import gspread
 from google.oauth2.service_account import Credentials
-import time
+import time, os
+
+TERMINAL_WIDTH = 80
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -21,10 +23,51 @@ class investment_game():
     the class of the game to be played
     """
     def __init__(self):
-        self.rounds = 5
+        self.ROUNDS = 5
         self.ptf_amount = 1000.00
         self.difficulty = ""
         self.rounds_data = {}
+
+    def menu(self):
+        self.ptf_amount = 1000.00
+        self.difficulty = ""
+        self.rounds_data = {}
+
+        print("                    ", end="")
+        print_with_attention("THE INVESTMENT GAME")
+        
+        print_throughout("_", False)
+        print_into_menu("MENU", True, True, True)
+        print_throughout("-", True)
+        
+        print_into_menu("Welcome to the investment game!", True, False, False)
+        print_throughout(" ", True)
+        print_into_menu(f"You have a portfolio of 3 securities and one cash account. You have to allocate {self.ptf_amount:.2f}€ to them, at the beginning of the game. You will play 5 rounds. Each round you can re-allocate your money and see how it gains profits! But be careful there are costs - Please read the rules! At the end, your final portfolio value will be ranked amongst other players. Are you be the best investor?", 
+            True, False, False)
+        print_throughout("-", True)
+        print_into_menu("Enter a number:", True, False, True)
+        print_throughout(" ", True)
+        print_into_menu("1. start", True, False, True)
+        print_into_menu("2. rules", True, False, False)
+        print_into_menu("3. rankings", True, False, False)
+        print_into_menu("4. exit game", True, False, False)
+        print_throughout("_", True)
+
+        menu_input = input("Enter a number (1 to 4):")
+        if self.validate_menu_input(menu_input):
+            os.system('cls' if os.name == 'nt' else 'clear')
+            self.menu()
+
+    def validate_menu_input(self, menu_input):
+        return False
+
+    def start(self):
+        """
+        Method that welcomes the player and shows the start menu
+        """
+        self.show_menu()
+        self.difficulty = self.set_difficulty()
+
 
     def set_difficulty(self):
         """
@@ -32,22 +75,78 @@ class investment_game():
         """
         diff = input("")
 
-    def start(self):
-        """
-        Method that welcomes the player, starts the game and fills the dictionary with data each round
-        """
-        print("")
-        self.difficulty = self.set_difficulty()
 
+    def play(self):
+        x = 5
+
+
+# printing functions ############################
 def print_with_attention(str):
     """
     function that prints a text slowly to gather attention - I like it 
     """
     for i in str:
-        print(i, end=" ")
-        time.sleep(0.3)
-        
-    time.sleep(1)
-    print()
+        print(f"\033[1;47;30m{i}\033[0m", end="\033[47;30m \033[0m")
+        time.sleep(0.1)
 
-print_with_attention("\033[1mTHE INVESTMENT GAME\033[0m\n")
+    time.sleep(0.1)
+    print("\n")
+
+def print_throughout(char, with_border):
+    """
+    This function prints one character throughout the the line, fitting into the menu format
+    suitable for borders 
+    """
+    total = TERMINAL_WIDTH
+    if with_border:
+        print("|", end="")
+        total -= 1
+    for i in range(total):
+        print(char, end="")
+    if with_border:
+        print("|", end="")
+    print("")
+
+def print_into_menu(str, with_border, centralized, bold):
+    """
+    This function prints a string that fits into the menu width and format 
+    """
+    total = TERMINAL_WIDTH
+    str_start = ""
+    str_bold_in = ""
+    str_bold_out = ""
+    str_centr_in = ""
+    str_centr_out = ""
+    str_end = ""
+
+    if with_border:
+        str_start = "|"
+        str_end = "|"
+        total -= 1
+    
+    if bold:
+        str_bold_in = "\033[1m"
+        str_bold_out = "\033[0m"
+
+    str = [str[i:i+total] for i in range(0, len(str), total)]
+
+    for sub_str in str:
+        if centralized:
+            str_centr_in = (total - len(sub_str)) // 2
+        else:
+            str_centr_in = 1
+        
+        str_centr_out = total-str_centr_in-len(sub_str)
+        if str_centr_out < 0:
+            str_centr_in = 0
+
+        str_centr_in *= " "
+        str_centr_out *= " "
+        print(str_start + str_centr_in + str_bold_in + sub_str + str_bold_out + str_centr_out + str_end)
+    
+
+    
+    
+
+game = investment_game()
+game.menu()
